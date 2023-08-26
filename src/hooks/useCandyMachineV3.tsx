47cb499d58e6b13404e8ca061ac4d6a0b34e2088
Jhonattan  Ramirez <53520083+camicasii@users.noreply.github.com>
2023-08-26 11:15:49 -0300

    Develop (#1)
    * use IMI

--- src/hooks/useCandyMachineV3.tsx
+++ src/hooks/useCandyMachineV3.tsx
@@ -36,6 +36,15 @@ import {
   parseGuardGroup,
   parseGuardStates,
 } from "./utils";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import { walletAdapterIdentity as walletAdapterIdentity2} from '@metaplex-foundation/umi-signer-wallet-adapters';
+import {  mintV2, mplCandyMachine, fetchCandyMachine as fetchCandyMachine2, fetchCandyGuard} from "@metaplex-foundation/mpl-candy-machine";
+import { generateSigner, publicKey, some, transactionBuilder } from "@metaplex-foundation/umi";
+import { setComputeUnitLimit ,createMintWithAssociatedToken,
+  findAssociatedTokenPda
+  
+  } from "@metaplex-foundation/mpl-toolbox";
+
 
 export default function useCandyMachineV3(
   candyMachineId: PublicKey | string,
@@ -110,7 +119,7 @@ export default function useCandyMachineV3(
       );
     const verifyProof = (
       merkleRoot: Uint8Array | string,
-      label = "default"
+      label = "early"
     ) => {
       let merkle = merkles[label];
       if (!merkle) return;
@@ -157,115 +166,163 @@ export default function useCandyMachineV3(
         nftGuards?: NftPaymentMintSettings[];
       } = {}
     ) => {
-      if (!guardsAndGroups[opts.groupLabel || "default"])
-        throw new Error("Unknown guard group label");
-
-      const allowList = opts.groupLabel &&
-        proofMemo.merkles[opts.groupLabel] && {
-          proof: proofMemo.merkles[opts.groupLabel].proof,
-        };
-
       let nfts: (Sft | SftWithToken | Nft | NftWithToken)[] = [];
-      try {
-        if (!candyMachine) throw new Error("Candy Machine not loaded yet!");
 
-        setStatus((x) => ({
-          ...x,
-          minting: true,
-        }));
+try {
 
-        const transactionBuilders: TransactionBuilder[] = [];
-        if (allowList) {
-          if (!proofMemo.merkles[opts.groupLabel || "default"].proof.length)
-            throw new Error("User is not in allowed list");
 
-          transactionBuilders.push(
-            callCandyGuardRouteBuilder(mx, {
-              candyMachine,
-              guard: "allowList",
-              group: opts.groupLabel,
-              settings: {
-                path: "proof",
-                merkleProof:
-                  proofMemo.merkles[opts.groupLabel || "default"].proof,
-              },
-            })
-          );
-        }
-        for (let index = 0; index < quantityString; index++) {
-          transactionBuilders.push(
-            await mintFromCandyMachineBuilder(mx, {
-              candyMachine,
-              collectionUpdateAuthority: candyMachine.authorityAddress, // mx.candyMachines().pdas().authority({candyMachine: candyMachine.address})
-              group: opts.groupLabel,
-              guards: {
-                nftBurn: opts.nftGuards && opts.nftGuards[index]?.burn,
-                nftPayment: opts.nftGuards && opts.nftGuards[index]?.payment,
-                nftGate: opts.nftGuards && opts.nftGuards[index]?.gate,
-                allowList,
-              },
-            })
-          );
-        }
-        const blockhash = await mx.rpc().getLatestBlockhash();
+//@ts-ignore
+const umi =createUmi(connection.rpcEndpoint)
+// const umi = createUmi("https://api.devnet.solana.com")
+.use(walletAdapterIdentity2(wallet))
+.use(mplCandyMachine());
+const candyMachinePublicKey = publicKey("5B1rkfFNoqPEufZzQ5woDYDFfANragpJ22Zi2UnvkC4J");
+const candyMachine_ = await fetchCandyMachine2(umi,candyMachinePublicKey);
+const candyGuard = await fetchCandyGuard(umi, candyMachine_.mintAuthority);
+const nftMint = generateSigner(umi);
 
-        const transactions = transactionBuilders.map((t) =>
-          t.toTransaction(blockhash)
-        );
-        const signers: { [k: string]: IdentitySigner } = {};
-        transactions.forEach((tx, i) => {
-          tx.feePayer = wallet.publicKey;
-          tx.recentBlockhash = blockhash.blockhash;
-          transactionBuilders[i].getSigners().forEach((s) => {
-            if ("signAllTransactions" in s) signers[s.publicKey.toString()] = s;
-            else if ("secretKey" in s) tx.partialSign(s);
-            // @ts-ignore
-            else if ("_signer" in s) tx.partialSign(s._signer);
-          });
-        });
-        let signedTransactions = transactions;
+// const tx  =await transactionBuilder()
+//   .add(setComputeUnitLimit(umi, { units: 800_000 }))
+//   // .add(createMintWithAssociatedToken(umi, { mint: buyer, owner: buyer.publicKey }))
+//   .add(
+//     mintV2(umi, {
+//       candyMachine: candyMachine_.publicKey,      
+//       nftMint,      
+//       collectionMint: candyMachine_.collectionMint,      
+//       collectionUpdateAuthority:candyMachine_.authority,      
+//       candyGuard: candyGuard.publicKey,          
+//       group:candyGuard.groups[0].label,    
+//       mintArgs: {
+//         mintLimit: some({ id: 1 }),
+//         //@ts-ignore
+//         solPayment: some({   destination: candyGuard.groups[0].guards.solPayment.value.destination.toString() }),
+        
+//         // botTax:some({ lamports: sol(0), lastInstruction: true }),
+        
+//     }
+//   }
+//     )
+//   )
+//   .sendAndConfirm(umi)
 
-        for (let signer in signers) {
-          signedTransactions = await signers[signer].signAllTransactions(transactions);
-        }
-        if (allowList) {
-          const allowListCallGuardRouteTx = signedTransactions.shift();
-          const allowListCallGuardRouteTxBuilder = transactionBuilders.shift();
-          await mx.rpc().sendAndConfirmTransaction(allowListCallGuardRouteTx, {
-            commitment: "processed",
-          });
-        }
-        const output = await Promise.all(
-          signedTransactions.map((tx, i) => {
-            return mx
-              .rpc()
-              .sendAndConfirmTransaction(tx, { commitment: "finalized" })
-              .then((tx) => ({
-                ...tx,
-                context: transactionBuilders[i].getContext() as any,
-              }));
-          })
-        );
-        nfts = await Promise.all(
-          output.map(({ context }) =>
-            mx
-              .nfts()
-              .findByMint({
-                mintAddress: context.mintSigner.publicKey,
-                tokenAddress: context.tokenAddress,
-              })
-              .catch((e) => null)
-          )
-        );
-        Object.values(guardsAndGroups).forEach((guards) => {
-          if (guards.mintLimit?.mintCounter)
-            guards.mintLimit.mintCounter.count += nfts.length;
-        });
-        // setItems((x) => ({
-        //   ...x,
-        //   remaining: x.remaining - nfts.length,
-        //   redeemed: x.redeemed + nfts.length,
-        // }));
+
+
+
+
+
+console.log(connection.rpcEndpoint,"asas");
+
+
+      return
+      // if (!guardsAndGroups[opts.groupLabel || "default"])
+      //   throw new Error("Unknown guard group label");
+
+      // const allowList = opts.groupLabel &&
+      //   proofMemo.merkles[opts.groupLabel] && {
+      //     proof: proofMemo.merkles[opts.groupLabel].proof,
+      //   };
+
+      
+      // try {
+      //   if (!candyMachine) throw new Error("Candy Machine not loaded yet!");
+
+      //   setStatus((x) => ({
+      //     ...x,
+      //     minting: true,
+      //   }));
+
+      //   const transactionBuilders: TransactionBuilder[] = [];
+      //   if (allowList) {
+      //     if (!proofMemo.merkles[opts.groupLabel || "default"].proof.length)
+      //       throw new Error("User is not in allowed list");
+
+      //     transactionBuilders.push(
+      //       callCandyGuardRouteBuilder(mx, {
+      //         candyMachine,
+      //         guard: "allowList",
+      //         group: opts.groupLabel,
+      //         settings: {
+      //           path: "proof",
+      //           merkleProof:
+      //             proofMemo.merkles[opts.groupLabel || "default"].proof,
+      //         },
+      //       })
+      //     );
+      //   }
+      //   for (let index = 0; index < quantityString; index++) {
+      //     transactionBuilders.push(
+      //       await mintFromCandyMachineBuilder(mx, {
+      //         candyMachine,
+      //         collectionUpdateAuthority: candyMachine.authorityAddress, // mx.candyMachines().pdas().authority({candyMachine: candyMachine.address})
+      //         group: opts.groupLabel,
+      //         guards: {
+      //           nftBurn: opts.nftGuards && opts.nftGuards[index]?.burn,
+      //           nftPayment: opts.nftGuards && opts.nftGuards[index]?.payment,
+      //           nftGate: opts.nftGuards && opts.nftGuards[index]?.gate,
+      //           allowList,
+      //         },
+      //       })
+      //     );
+      //   }
+      //   const blockhash = await mx.rpc().getLatestBlockhash();
+
+      //   const transactions = transactionBuilders.map((t) =>
+      //     t.toTransaction(blockhash)
+      //   );
+      //   const signers: { [k: string]: IdentitySigner } = {};
+      //   transactions.forEach((tx, i) => {
+      //     tx.feePayer = wallet.publicKey;
+      //     tx.recentBlockhash = blockhash.blockhash;
+      //     transactionBuilders[i].getSigners().forEach((s) => {
+      //       if ("signAllTransactions" in s) signers[s.publicKey.toString()] = s;
+      //       else if ("secretKey" in s) tx.partialSign(s);
+      //       // @ts-ignore
+      //       else if ("_signer" in s) tx.partialSign(s._signer);
+      //     });
+      //   });
+      //   let signedTransactions = transactions;
+
+      //   for (let signer in signers) {
+      //     signedTransactions = await signers[signer].signAllTransactions(transactions);
+      //   }
+      //   if (allowList) {
+      //     const allowListCallGuardRouteTx = signedTransactions.shift();
+      //     const allowListCallGuardRouteTxBuilder = transactionBuilders.shift();
+      //     await mx.rpc().sendAndConfirmTransaction(allowListCallGuardRouteTx, {
+      //       commitment: "processed",
+      //     });
+      //   }
+      //   const output = await Promise.all(
+      //     signedTransactions.map((tx, i) => {
+      //       return mx
+      //         .rpc()
+      //         .sendAndConfirmTransaction(tx, { commitment: "finalized" })
+      //         .then((tx) => ({
+      //           ...tx,
+      //           context: transactionBuilders[i].getContext() as any,
+      //         }));
+      //     })
+      //   );
+      //   nfts = await Promise.all(
+      //     output.map(({ context }) =>
+      //       mx
+      //         .nfts()
+      //         .findByMint({
+      //           mintAddress: context.mintSigner.publicKey,
+      //           tokenAddress: context.tokenAddress,
+      //         })
+      //         .catch((e) => null)
+      //     )
+      //   );
+      //   Object.values(guardsAndGroups).forEach((guards) => {
+      //     if (guards.mintLimit?.mintCounter)
+      //       guards.mintLimit.mintCounter.count += nfts.length;
+      //   });
+      //   // setItems((x) => ({
+      //   //   ...x,
+      //   //   remaining: x.remaining - nfts.length,
+      //   //   redeemed: x.redeemed + nfts.length,
+      //   // }));
       } catch (error: any) {
         let message = error.msg || "Minting failed! Please try again!";
         if (!error.msg) {
@@ -386,9 +443,9 @@ export default function useCandyMachineV3(
     // };
     return Object.entries(guardsAndGroups).reduce(
       (groupPayments, [label, guards]) => {
-        // console.log(label, guards);
+        console.log(label, guards);
         return Object.assign(groupPayments, {
-          [label]: guardToPaymentUtil(guards),
+          ["early"]: guardToPaymentUtil(guards),
         });
       },
       {}
